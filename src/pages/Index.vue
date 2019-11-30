a40d3938d6ff5cbdb47d0e516b2eee3926a0a1bb
a_t_ruby <a_t_ruby@yahoo.com>
2019-11-30 10:54:08 +0800

    Update welcome text.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,7 @@
 <template>
   <q-page class="flex flex-center column q-gutter-sm">
-    <div>
-      This is an experimental tool for live broadcasting. Please watch this
-      <a href="https://www.youtube.com/watch?v=VrbYwSsnuKo" target="_blank">a short introduction</a>.
-    </div>
-    <div>
-      We would appriciate if you could help us improve this tool by providing us your <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=drd2NJDpck-5UGJImDFiPdy_BsD-D2tLtQLYkFJDRaZURFdJUlhTT1g5VkpFQjRLS1hDTThFUFJQRi4u" target="_blank">background</a> before testing and <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=drd2NJDpck-5UGJImDFiPdy_BsD-D2tLtQLYkFJDRaZUNUZFQVVJRkpCTVRBNjQ2RjZXUzkxVFJKQS4u" target="_blank">feedback</a> after the testing. Please use ID {{ id }} for both forms.
-    </div>
-    <div>-----</div>
     <div><input type="text" v-model="name" placeholder="Enter Key" /></div>
-    <div>The key can be any unique text. It is used for pairing the application and the controller.</div>
+    <div>Any unique text for pairing the application and the controller.</div>
     <div class="q-mt-md">Design A</div>
     <div>
       <q-btn @click="open('app/A')">Application</q-btn>
@@ -21,8 +13,12 @@
       <q-btn @click="open('control/B')">Controller</q-btn>
     </div>
     <div>-----</div>
-    <div>This is a prototype, which may not work in your devices.</div>
-    <div>Developed by Atima Tharatipyakul.</div>
+    <div>This is an experimental tool for live broadcasting.</div>
+    <div>
+      I would appreciate if you could contact me at atima_tharatipyakul
+      <img src="statics/sutd.png" style="height: 1em" /> before trying it.
+    </div>
+    <div>This way, you could provide us useful insights, which will influence R&D of broadcasting tools of the future.</div>
   </q-page>
 </template>
 
